08a89f1b20d6bbd5a1cb3f4684b42dd9d68d2281
amin boutarfi <mr.amineboutarfi@gmail.com>
2024-07-24 18:51:19 +0100

    specify the secrets path

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from celery import Celery
 from celery.utils.log import get_task_logger
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('/app/.env')
 
 logger = get_task_logger(__name__)
 # Configure the logs
